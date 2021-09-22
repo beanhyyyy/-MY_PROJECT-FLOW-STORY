71dbf1e04a9243a5a7c1e2ff7ad3624634ea67c5
nguyen <quangnguyenne1411@gmail.com>
2021-09-22 14:43:17 +0700

    [UPDATE] Animation Text

--- app/containers/Page/SchoolPage/index.js
+++ app/containers/Page/SchoolPage/index.js
@@ -12,7 +12,47 @@ import HahaloloLogoIcon from "icons/HahaloloLogoIcon";
 import imageHHLL from "images/imageHHLL.jpg";
 import AtomRouterLink from "../../../Atomic/atoms/RouterLink";
 
-const useStyle = makeStyles((theme) => ({}));
+const useStyle = makeStyles((theme) => ({
+  styleWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+  },
+  styleStaticText: {
+    color: "#fff",
+    fontSize: "24px",
+    fontWeight: 400,
+  },
+  styleDynamicText: {
+    position: "relative",
+  },
+  styleSpan: {
+    color: "#ea4335",
+    listStyle: "none",
+    fontSize: "24px",
+    fontWeight: 500,
+    "&:after": {
+      content: '""',
+      position: "absolute",
+      left: 0,
+      width: "100%",
+      height: "100%",
+      // borderLeft: "2px solid white",
+      animation: "$typing 2s steps(10)",
+    },
+  },
+  "@keyframes typing": {
+    "0%": {
+      borderLeft: "2px solid #ea4335",
+    },
+    "100%": {
+      borderLeft: "2px solid #ea4335",
+      left: "90%",
+      margin: "0px -35px 0px 35px",
+    },
+  },
+}));
 
 export default function SchoolPage() {
   const classes = useStyle();
@@ -24,12 +64,10 @@ export default function SchoolPage() {
       </Helmet>
       <div>
         <div className={classes.styleWrapper}>
-          <div className={classes.styleStaticText}>i'm</div>
-          <ul className={classes.styleDynamicText}>
-            <li>
-              <span>12312312321 12414 124 12 41</span>
-            </li>
-          </ul>
+          <div className={classes.styleStaticText}>i'm&nbsp;</div>
+          <span className={classes.styleDynamicText}>
+            <span className={classes.styleSpan}>123123123 21321</span>
+          </span>
         </div>
         <PageBackground image={imageHHLL} />
       </div>
